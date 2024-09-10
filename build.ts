930eb42b2eb71bd82bd37e63a4b3d53d78a1d3da
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-11 08:28:46 +0900

    move main file to src/

--- build.ts
+++ build.ts
@@ -1,7 +1,7 @@
 import UnpluginIsolatedDecl from "unplugin-isolated-decl/esbuild"
 
 await Bun.build({
-entrypoints: ['./index.ts'],
-outdir: "./dist",
-plugins: []
+  entrypoints: ["./src/index.ts"],
+  outdir: "./dist",
+  plugins: [],
 })
