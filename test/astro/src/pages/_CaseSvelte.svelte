e54f3da0eed53ac9e5ea1a836491cb46ed7cb79f
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-11 21:00:28 +0900

    playground: refactor Astro case into its own file

--- test/astro/src/pages/_CaseSvelte.svelte
+++ test/astro/src/pages/_CaseSvelte.svelte
@@ -4,7 +4,7 @@
 
 <div>
   <div class="inputHeader">This value should be synced up with the q param</div>
-  <input type="search" bind:value={$q} placeholder="Text..." />
+  <input type="search" bind:value={$q} placeholder="Type some text here..." />
   <div>
     <button
       on:click={() => {
