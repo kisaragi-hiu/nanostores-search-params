b9cceb0b59d335406bf6985724f7f0c9a3d17f6b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-09-11 11:59:20 +0900

    Add sveltekit playground

---
+++ test/sveltekit/README.md
@@ -0,0 +1,9 @@
+# playground for nanostores-search-params in sveltekit
+
+Run:
+
+```sh
+bun install
+bun dev
+# then open http://localhost:5173/
+```
---
+++ test/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { queryParam } from "../../../src/index";
+  import { page } from "$app/stores";
+  const name = queryParam("name", { url: $page.url });
+</script>
+
+<h1>nanostores-search-params playground</h1>
+<button
+  on:click={() => {
+    $name = "A";
+  }}>Set to "A"</button
+>
+<button
+  on:click={() => {
+    $name = "B";
+  }}>Set to "B"</button
+>
+<p>Current name: {$name}</p>
